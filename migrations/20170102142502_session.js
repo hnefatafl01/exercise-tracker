7620529f66068aea1342268955e9ff2c5dca6148
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-01-02 16:44:47 -0700

    initial commit

---
+++ migrations/20170102142502_session.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('session', (table)=> {
+    table.increments();
+    table.date('date');
+    table.text('exercise');
+    table.integer('duration');
+    table.integer('distance');
+    table.string('HR-zone');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('session');
+};
