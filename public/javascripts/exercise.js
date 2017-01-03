0f93e52c5f79984878a00543796fa0574b0aa4ab
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-01-02 17:38:51 -0700

    form interactive

--- public/javascripts/exercise.js
+++ public/javascripts/exercise.js
@@ -1,3 +1,32 @@
-$(document).ready(()=> {
+// const exercise = $('#exercise');
+// const  = $('#low');
+// const  = $('#med');
+// const  = $('#high');
+// const  = $('#max');
+const listItem = $('.list-group>li');
 
+$(document).ready(()=> {
+  listHover();
+  selectHR();
 })
+
+function listHover() {
+  listItem.hover(
+  function() {
+    $(this).css('background', '#0769ad')
+  },
+  function() {
+    $(this).css('background', '#222222')
+  })
+}
+
+function selectHR() {
+  listItem.click(function() {
+    const selected = $(this).hasClass('active')
+    if(selected) {
+      $(this).removeClass('active')
+    } else {
+      $(this).addClass('active')
+    }
+  })
+}
